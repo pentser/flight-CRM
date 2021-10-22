199e41447abb5b8bb69140dab973172018a28eb7
eli p <eli.pentser@gmail.com>
2021-10-22 14:19:11 +0300

    update search form

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import {LoginApi} from '../../services/login-api';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginApi:LoginApi,private router:Router) { }
+
+  onSub:Subscription=null;
+  onSubmit(form) {
+
+
+    if(form.status=="VALID")
+    {
+      console.log("form:",form.value);
+      this.onSub=this.loginApi.getLogin(form.value).subscribe((data)=>{
+        console.log(data);
+        if(data)
+        {
+        /* for (const key in data)
+             alert(data[key]); */
+             console.log(data);
+            this.router.navigate(['/flights'])
+        }
+
+      })
+
+    }
+
+    else{
+      console.log('invalid data:',form);
+    }
+
+  }
+
 
   ngOnInit(): void {
   }
